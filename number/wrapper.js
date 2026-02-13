ec4cad7d931d6260bab6029b1ace389f94a8433e
Nikos.p <connor2000@hotmail.gr>
2026-02-13 10:27:31 +0200

    feat: add wrapper examples

---
+++ number/wrapper.js
@@ -0,0 +1,4 @@
+console.log(Number.MAX_VALUE); // Maximum representable number
+console.log(Number.MIN_VALUE); // Minimum representable number
+
+console.log(Number.parseInt("34"))  // Positive infinity
