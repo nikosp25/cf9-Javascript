4e0f499452ee2a7c4457d5b2e259443fa4f8f136
Nikos.p <connor2000@hotmail.gr>
2026-02-21 17:04:53 +0200

    feat: add destructure, JSON, and object methods examples.

---
+++ objects/json2.js
@@ -0,0 +1,16 @@
+const jsonStr = ` {
+    "id": 1,
+    "firstname": "John",
+    "lastname": "Smith",
+    "age": 30,
+    "isTeacher": false,
+    "hobbies": ["football", "music"],
+    "address": {
+        "city": "Athens",
+        "country": "Greece",
+        "zip": "12345"
+    },
+    "phoneNumber": null
+}`
+
+console.log(JSON.parse(jsonStr)) // deserialization 
